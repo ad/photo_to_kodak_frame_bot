f9ebc948d3f88d3bbfd391138be5f5acffd55eaa
Daniel Apatin <daniel@apatin.ru>
2024-11-17 14:50:30 +0300

    test

--- functions/webhook/[path].js
+++ functions/webhook/[path].js
@@ -1,32 +1,39 @@
 import { EmailMessage } from "cloudflare:email";
-import { createMimeMessage } from "../utils/gas.js";
+// import { createMimeMessage } from "../utils/gas.js";
 
 export function onRequest(context) {
   console.log("context.request", context.request);
 
-  return sendEmail(context, []);
+  try {
+    sendEmail(context.env, "photo@workerdev.ru", "photo@apatin.ru", "test", "body");
+    return new Response('Email sent successfully');
+  } catch (e) {
+    return new Response(`Internal Server Error: ${e.message}`, { status: 500 });
+  }
 }
 
-async function sendEmail(context, params) {
-  const msg = createMimeMessage();
-  msg.setSender({ name: "test", addr: "photo@workerdev.ru" });
-  msg.setRecipient("photo@apatin.ru");
-  msg.setSubject("An email generated in a worker");
-  msg.addMessage({
-      contentType: 'text/plain',
-      data: `Congratulations, you just sent an email from a worker.`
-  });
+async function sendEmail(env, from, to, subject, html) {
+  const rawMessage = buildRawEmailMessage(from, to, subject, html);
 
-  var message = new EmailMessage(
-    "photo@workerdev.ru",
-    "photo@apatin.ru",
-    msg.asRaw()
+  const message = new EmailMessage(
+    from,
+    to,
+    rawMessage
   );
-  try {
-    await env.SEB.send(message);
-  } catch (e) {
-    return new Response(e.message);
-  }
 
-  return new Response("Hello Send Email World!");
+  await env.EMAIL.send(message);
+}
+
+function buildRawEmailMessage(from, to, subject, html) {
+  const headers = [
+    'MIME-Version: 1.0',
+    `From: ${from}`,
+    `To: ${to}`,
+    `Subject: ${subject}`,
+    'Content-Type: text/html; charset=UTF-8',
+    '',
+    html
+  ];
+
+  return headers.join('\r\n');
 }
